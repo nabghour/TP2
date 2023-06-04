01e0895817fc39bbf7133e392f12a81aede54664
Noreddine Abghour <nabghour@gmail.com>
2023-06-04 12:29:50 +0100

    Update essai.sh 1

--- essai.sh
+++ essai.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo " Bonjour je suis le Job J2"
-echo "la date d'aujourd'hui est : `date`" 
-echo "au revoir"
+echo " ********** Bonjour je suis le Job J2 ************"
+echo " ********** la date d'aujourd'hui est : `date` ***************" 
+echo " ********** au revoir **********"
