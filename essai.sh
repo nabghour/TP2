d55133eac46e6c91d0e741bcdf3680549570f037
Noreddine Abghour <nabghour@gmail.com>
2023-06-04 12:42:25 +0100

    Update essai.sh 3

--- essai.sh
+++ essai.sh
@@ -2,3 +2,4 @@
 echo " ********** Bonjour je suis le Job J2"
 echo " ********** la date d'aujourd'hui est : `date`" 
 echo " ********** au revoir"
+echo " #############" je vais declancher le job 3 ###############" 
